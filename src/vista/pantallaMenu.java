6b568bb7a0d56991e49845b0b0d6fd2fd211e9f8
alex24050714 <alexmontserrat2405a@alumnes.ilerna.com>
2025-04-03 15:37:50 +0200

    estructura

--- src/vista/pantallaMenu.java
+++ src/vista/pantallaMenu.java
@@ -1,5 +1,19 @@
 package vista;
 
 public class pantallaMenu {
-
+	public void menu () {
+		
+	}
+	
+	public void botoNovaPartida () {
+		
+	}
+	
+	public void botoCarregarPartida () {
+		
+	}
+	
+	public void botoSortir () {
+		
+	}
 }
--- src/vista/pantallaPartida.java
+++ src/vista/pantallaPartida.java
@@ -2,4 +2,27 @@ package vista;
 
 public class pantallaPartida {
 
+	public void botoTirarDau() {
+		
+	}
+	
+	public void botoUtilitzarObjecte () {
+		
+	}
+	
+	public void botoFinalitzarTorn () {
+		
+	}
+	
+	public void iniciarPartida () {
+		
+	}
+	
+	public void carregarPartida () {
+		
+	}
+	
+	public void guardarPartida () {
+		
+	}
 }
