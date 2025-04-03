01fe16a2ace10ca7747cb383b99d28bd74bae32c
Your Name <you@example.com>
2025-04-03 16:55:02 +0200

    controlador

--- src/controlador/gestorPartides.java
+++ src/controlador/gestorPartides.java
@@ -1,5 +1,34 @@
 package controlador;
 
 public class gestorPartides {
+	private String linkBBDD;
+	private String username;
+	private String password;
+	
+	public gestorPartides(String linkBBDD, String username, String password) {
+		this.linkBBDD=linkBBDD;
+		this.username=username;
+		this.password=password;
+	}
+	public String getLinkBBDD() {
+		return linkBBDD;
+	}
+	public void setLinkBBDD(String linkBBDD) {
+		this.linkBBDD = linkBBDD;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 
 }
--- src/controlador/gestorTablero.java
+++ src/controlador/gestorTablero.java
@@ -1,6 +1,17 @@
 package controlador;
 
 public class gestorTablero {
-	
+	public void actualitzarMovJug(Jugador j) {
+		
+	}
+	public void accioCasella(Pingüi p,Casella c) {
+		
+	}
+	public void actualitzaFinalitzarTorn() {
+		
+	}
+	public void actualitzarPantalla() {
+		
+	}
 	
 }
